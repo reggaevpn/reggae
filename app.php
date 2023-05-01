ecd1814f84b074ba9d603a9feb8e9bf8e0395d86
reggaevpn <120673752+reggaevpn@users.noreply.github.com>
2023-05-02 02:32:12 +0700

    Update app.php

--- app.php
+++ app.php
@@ -2,6 +2,7 @@
   "Boolean": true,
   "GooglePlay": "https://play.google.com/store/apps/details?id=com.publicreggaevpn.reggaevpn",
   "Message": "โปรดอ่านรายละเอียด
+
 กรุณากดอัปเดตแอปพลิเคชั่นให้เป็นเวอชั่นปัจจุบัน 
 กดคำว่า ติดตั้ง แล้วกดอัปเดต"
 }
