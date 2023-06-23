1b83dbf6957ecd5f41225253e62f1f67e8e52508
reggaevpn <120673752+reggaevpn@users.noreply.github.com>
2023-06-23 16:38:36 +0700

    Update app.php

--- app.php
+++ app.php
@@ -1,10 +1,8 @@
 {
   "Boolean": false,
-  "GooglePlay": "https://play.google.com/store/apps/details?id=com.publicreggaevpn.reggaevpn",
+  "GooglePlay": "https://play.google.com/store/apps/details?id=com.publicunc.uncvpn",
   "Message": "โปรดอ่านรายละเอียด
 
-แอปพลิเคชั่นยุติให้การชั่วคราว
-กรุณากดติดตั้งเพื่อย้ายไปยังแอปพลิเคชั่นตัวใหม่
-หากสมาชิกติดตั้งแอปพลิเคชั่นใหม่แล้ว
-ให้ทำการลบแอปพลิเคชั่นนี้ได้เลย"
+แอปพลิเคชั่นปิดปรับปรุงชั่วคราว
+กรุณากดติดตั้งเพื่อย้ายไปยังแอปพลิเคชั่นตัวใหม่"
 }
