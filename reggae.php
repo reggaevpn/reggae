01c4a9e205571fb1b439cc32f8ce4a0fa59ac957
reggaevpn <120673752+reggaevpn@users.noreply.github.com>
2023-12-12 01:43:13 +0700

    Update reggae.php

--- reggae.php
+++ reggae.php
@@ -13,16 +13,16 @@
 ✔️กันรั่ว Ais กด *777*7067# 30วัน 31บ./เดือน ",
   "data": [
     {
-      "name": "TH 01_AIS-PLAY+กันเน็ตรั่ว",
+      "name": "TH 01_AIS-PLAY",
       "detail": "โปร AisPlay ใช้คู่กับกันเน็ตรั่ว",
       "content": "vmess://ewogICJ2IjogIjIiLAogICJwcyI6ICJ1d3czb3JoaCIsCiAgImFkZCI6ICIxMDMuODIuMjQ5Ljc4IiwKICAicG9ydCI6IDgwODAsCiAgImlkIjogIjFkZTE2YjA0LTljYTAtNGZhNC04Yjc3LWJmOWE1YjBkODlkMiIsCiAgIm5ldCI6ICJ0Y3AiLAogICJ0eXBlIjogImh0dHAiLAogICJ0bHMiOiAibm9uZSIsCiAgInBhdGgiOiAiLyIsCiAgImhvc3QiOiAic3BlZWR0ZXN0Lm5ldCIKfQ=="
     }, {
-      "name": "TH DTAC-01 ชื่อไทยได้",
-      "detail": "รายละเอียด TH 01",
+      "name": "TH 01_DTAC-แฮปปี้ไลน์",
+      "detail": "โปรแฮปปี้ไลน์ใช้คู่กับกันเน็ตรั่ว",
       "content": "vmess://eyJhZGQiOiIxMDMuODIuMjQ5Ljc4IiwiYWlkIjoiMCIsImFscG4iOiIiLCJmcCI6IiIsImhvc3QiOiJtLndlYnRvb25zLmNvbSIsImlkIjoiMWRlMTZiMDQtOWNhMC00ZmE0LThiNzctYmY5YTViMGQ4OWQyIiwibmV0IjoidGNwIiwicGF0aCI6Ii8iLCJwb3J0IjoiODA4MCIsInBzIjoidXd3M29yaGgiLCJzY3kiOiJhdXRvIiwic25pIjoiIiwidGxzIjoiIiwidHlwZSI6Imh0dHAiLCJ2IjoiMiJ9"
     }, {
-      "name": "TH TRUE-01 ชื่อไทยได้",
-      "detail": "รายละเอียด TH 01",
+      "name": "TH 01_TRUE-โนโปร",
+      "detail": "ทรูโนโปรไม่ต้องสมัครโปร",
       "content": "vmess://eyJhZGQiOiIwOjA6MDowOjA6RkZGRjo2ODEwOjU1MTQiLCJhaWQiOiIwIiwiYWxwbiI6IiIsImZwIjoiIiwiaG9zdCI6InZ2LjJpbjF2cG4ubmV0IiwiaWQiOiI5M2UwNzMyZC00OWE0LTQ0MDQtOWY3Ni04NWFhM2ZhODU5MGQiLCJuZXQiOiJ3cyIsInBhdGgiOiIvIiwicG9ydCI6IjgwIiwicHMiOiJMaW5lLTQ1Y3hqeHRvIiwic2N5IjoiYXV0byIsInNuaSI6IiIsInRscyI6IiIsInR5cGUiOiIiLCJ2IjoiMiJ9"
         }
   ]
