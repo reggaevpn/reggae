7c92c1be494c8ed77482c7bb4339cb46b3a333b2
reggaevpn <120673752+reggaevpn@users.noreply.github.com>
2023-12-21 06:59:39 +0700

    Update reggae.php

--- reggae.php
+++ reggae.php
@@ -2,24 +2,16 @@
   "version": 1.1,
   "message": "
 🔴ทรูไม่ต้องสมัครโปร
-🔵ซิม DTAC
-⬇️โปร HappyLINE ใช้คู่กับกันรั่ว
-✔️HappyLine กด *104*777# 53บ./เดือน
-✔️กันรั่วดีเทค กด *117*30# 20บ. 30วัน
-
+🔵ซิม DTAC🚫ยังใช้ไม่ได้
 🟢ซิม AIS
 ⬇️โปร AisPlay ใช้คู่กับกันรั่ว
 ✔️AisPlay กด *777*885# 30วัน 64 บ./เดือน
 ✔️กันรั่ว Ais กด *777*7067# 30วัน 31บ./เดือน ",
   "data": [
     {
-      "name": "TH 01_AIS-PLAY",
+      "name": "TH AIS-PLAY_01",
       "detail": "โปร AisPlay ใช้คู่กับกันเน็ตรั่ว",
       "content": "vmess://ewogICJ2IjogIjIiLAogICJwcyI6ICJ1d3czb3JoaCIsCiAgImFkZCI6ICIxMDMuODIuMjQ5Ljc4IiwKICAicG9ydCI6IDgwODAsCiAgImlkIjogIjFkZTE2YjA0LTljYTAtNGZhNC04Yjc3LWJmOWE1YjBkODlkMiIsCiAgIm5ldCI6ICJ0Y3AiLAogICJ0eXBlIjogImh0dHAiLAogICJ0bHMiOiAibm9uZSIsCiAgInBhdGgiOiAiLyIsCiAgImhvc3QiOiAic3BlZWR0ZXN0Lm5ldCIKfQ=="
-    }, {
-      "name": "TH 01_DTAC-แฮปปี้ไลน์",
-      "detail": "โปรแฮปปี้ไลน์ใช้คู่กับกันเน็ตรั่ว",
-      "content": "vmess://eyJhZGQiOiIxMDMuODIuMjQ5Ljc4IiwiYWlkIjoiMCIsImFscG4iOiIiLCJmcCI6IiIsImhvc3QiOiJtLndlYnRvb25zLmNvbSIsImlkIjoiMWRlMTZiMDQtOWNhMC00ZmE0LThiNzctYmY5YTViMGQ4OWQyIiwibmV0IjoidGNwIiwicGF0aCI6Ii8iLCJwb3J0IjoiODA4MCIsInBzIjoidXd3M29yaGgiLCJzY3kiOiJhdXRvIiwic25pIjoiIiwidGxzIjoiIiwidHlwZSI6Imh0dHAiLCJ2IjoiMiJ9"
     }, {
       "name": "TH 01_TRUE-โนโปร",
       "detail": "ทรูโนโปรไม่ต้องสมัครโปร",
