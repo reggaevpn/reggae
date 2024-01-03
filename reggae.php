ba5fca08877b12b4ff0349ff90d5f695febc203a
reggaevpn <120673752+reggaevpn@users.noreply.github.com>
2024-01-03 08:19:07 +0700

    Update reggae.php

--- reggae.php
+++ reggae.php
@@ -15,11 +15,11 @@
       "content": "vless://e2911184-2a22-4b98-bdb1-52062e1521bc@th1.2in1vpn.net:8080?type=tcp&path=%2F&host=www.speedtest.net&headerType=http&security=none#th1ais"
     }, {
       "name": "TH TRUE-NOPRO_01",
-      "detail": "ทรูโนโปรเน็ตไม่ค่อยสเถียรใช้งานทั่วไปได้",
+      "detail": "ทรูโนโปร/ไม่ค่อยสเถียรใช้งานทั่วไปได้",
       "content": "vless://25a0214e-eb2e-4e31-a988-ba5095fc089c@truefree.2in1vpn.net:80?path=%2F&security=none&encryption=none&host=th1.2in1vpn.net&type=ws#th1truenopro"
     }, {
       "name": "TH TRUE-เฟสบุ๊คเกมมิ่ง_01",
-      "detail": "ทรูเฟสบุ๊คเกมมิ่งเน็ตสเถียรใช้งานทั่วไปได้",
+      "detail": "ทรูเฟสบุ๊คเกมมิ่ง/สเถียรใช้งานทั่วไปได้",
       "content": "vless://3d752da5-6f68-4138-8861-b8d186c87fef@th1.2in1vpn.net:445?security=tls&encryption=none&alpn=http/1.1&headerType=http&type=tcp&sni=connect.facebook.com#th1face"
     }
   ]
