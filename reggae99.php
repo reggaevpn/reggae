63090fceeff586432d2c0fdce684ec70f56db0ad
reggaevpn <120673752+reggaevpn@users.noreply.github.com>
2024-01-04 20:42:50 +0700

    Update reggae99.php

---
+++ reggae99.php
@@ -0,0 +1,30 @@
+{
+  "version": 1.1,
+  "message": "
+🇹🇭Thailand 10 Sever
+🌐Support AIS,TRUE-MOVEH
+🔴ทรูไม่ต้องสมัครโปร 
+🟢ซิม AIS
+➡️โปร AisPlay สมัครคู่กับกันรั่วจึงจะใช้งานได้
+📞AisPlay กด *777*885# 30วัน 64 บ./เดือน
+📞กันรั่ว Ais กด *777*7067# 30วัน 31บ./เดือน ",
+  "data": [
+    {
+      "name": "TH AIS-PLAY_01",
+      "detail": "Ais-Play ต้องใช้คู่กับกันรั่ว",
+      "content": "vless://e2911184-2a22-4b98-bdb1-52062e1521bc@th1.2in1vpn.net:8080?type=tcp&path=%2F&host=www.speedtest.net&headerType=http&security=none#th1ais"
+    }, {
+      "name": "TH DTAC-ยังใช้ไม่ได้_01",
+      "detail": "ดีเทคโนโปรใช้งานทั่วไป",
+      "content": "vless://25a0214e-eb2e-4e31-a988-ba5095fc089c@truefree.2in1vpn.net:80?path=%2F&security=none&encryption=none&host=th1.2in1vpn.net&type=ws#th1truenopro"
+    }, {
+      "name": "TH TRUE-NOPRO_01",
+      "detail": "ทรูโนโปรใช้งานทั่วไป",
+      "content": "vless://25a0214e-eb2e-4e31-a988-ba5095fc089c@truefree.2in1vpn.net:80?path=%2F&security=none&encryption=none&host=th1.2in1vpn.net&type=ws#th1truenopro"
+    }, {
+      "name": "TH TRUE-เฟสบุ๊คเกมมิ่ง_01",
+      "detail": "ทรูเฟสบุ๊คเกมมิ่งใช้งานทั่วไป",
+      "content": "vless://3d752da5-6f68-4138-8861-b8d186c87fef@th1.2in1vpn.net:433?type=grpc&serviceName=iop&security=tls&fp=&alpn=http%2F1.1&allowInsecure=1&sni=m.facebook.com#th1face"
+    }
+  ]
+}
